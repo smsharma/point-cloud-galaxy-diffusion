75f8598fd1d9a345c919de825713c926baf6f06d
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-01-25 22:55:36 -0500

    Removed unconditional dropout and likelihood eval for now

--- models/diffusion.py
+++ models/diffusion.py
@@ -76,7 +76,7 @@ class ScoreNet(nn.Module):
 
         t_embedding = get_timestep_embedding(t, self.d_t_embedding)  # Timestep embeddings
         cond = np.concatenate([t_embedding, conditioning], axis=1)  # Concatenate with conditioning context
-
+        
         h = Transformer(n_input=self.d_embedding, **self.transformer_dict)(z, cond, mask)
 
         return z + h
@@ -96,7 +96,6 @@ class VariationalDiffusionModel(nn.Module):
     latent_diffusion: bool = False
     d_t_embedding: int = 32
     transformer_dict: dict = dataclasses.field(default_factory=lambda: {"d_model": 256, "d_mlp": 512, "n_layers": 4, "n_heads": 4, "flash_attention": True})
-    p_uncond: float = 0.1
     n_classes: int = 0
 
     def setup(self):
@@ -114,7 +113,8 @@ class VariationalDiffusionModel(nn.Module):
         self.score_model = ScoreNet(d_t_embedding=self.d_t_embedding, d_embedding=embedding_dim, transformer_dict=self.transformer_dict)
         self.encoder = Encoder(d_hidden=self.d_hidden_encoding, n_layers=self.n_layers, d_embedding=embedding_dim, latent_diffusion=self.latent_diffusion)
         self.decoder = Decoder(d_hidden=self.d_hidden_encoding, n_layers=self.n_layers, d_output=self.d_feature, noise_scale=self.noise_scale, latent_diffusion=self.latent_diffusion)
-
+        
+        # Embedding for class and context
         if self.n_classes > 0:
             self.embedding_class = nn.Embed(self.n_classes, self.d_hidden_encoding)
         self.embedding_context = nn.Dense(self.d_hidden_encoding)
@@ -126,7 +126,6 @@ class VariationalDiffusionModel(nn.Module):
         """The reconstruction loss measures the gap in the first step.
         We measure the gap from encoding the image to z_0 and back again.
         """
-        ### Reconsturction loss 2
         g_0 = self.gamma(0.0)
         eps_0 = jax.random.normal(self.make_rng("sample"), shape=f.shape)
         z_0 = variance_preserving_map(f, g_0, eps_0)
@@ -139,7 +138,6 @@ class VariationalDiffusionModel(nn.Module):
         divergence between the final sample from the forward process and starting
         distribution for the reverse process, here taken to be a N(0,1).
         """
-        # KL z1 with N(0,1) prior
         g_1 = self.gamma(1.0)
         var_1 = sigma2(g_1)
         mean1_sqr = (1.0 - var_1) * np.square(f)
@@ -168,20 +166,7 @@ class VariationalDiffusionModel(nn.Module):
     def __call__(self, x, conditioning=None, mask=None):
 
         d_batch = x.shape[0]
-
-        # Unconditional dropout
-        # Set a random number of conditionining vectors to zero for unconditional modeling
-        if self.p_uncond and conditioning is not None:
-            rng_uncond = self.make_rng("uncond")
-            p_rnd = jax.random.uniform(rng_uncond, (conditioning.shape[0],))
-            mask_dropout = p_rnd < self.p_uncond
-
-            # Set context to -1 with random dropout
-            if self.n_classes == 0:
-                conditioning = np.where(mask_dropout[:, np.newaxis], -1.0 * np.ones_like(conditioning), conditioning)
-            else:  # If we have classes, only set non-class attributes to -1
-                conditioning.at[:, 1:].set(np.where(mask_dropout[:, np.newaxis], -1.0 * np.ones_like(conditioning[:, 1:]), conditioning[:, 1:]))
-
+                                
         # 1. Reconstruction loss
         # Add noise and reconstruct
         f = self.encode(x, conditioning)
@@ -236,7 +221,7 @@ class VariationalDiffusionModel(nn.Module):
             cond = None
         return self.decoder(z0, cond)
 
-    def sample_step(self, rng, i, T, z_t, conditioning, mask=None, guidance_weight=0.0):
+    def sample_step(self, rng, i, T, z_t, conditioning=None, mask=None):
         """Sample a single step of the diffusion process."""
         rng_body = jax.random.fold_in(rng, i)
         eps = jax.random.normal(rng_body, z_t.shape)
@@ -249,14 +234,11 @@ class VariationalDiffusionModel(nn.Module):
         cond = self.embed(conditioning)
 
         eps_hat_cond = self.score_model(z_t, g_t * np.ones((z_t.shape[0],), z_t.dtype), cond, mask)
-        eps_hat_uncond = self.score_model(z_t, g_t * np.ones((z_t.shape[0],), z_t.dtype), cond * 0.0, mask)
-
-        eps_hat = eps_hat_cond * (1.0 + guidance_weight) - guidance_weight * eps_hat_uncond
 
         a = nn.sigmoid(g_s)
         b = nn.sigmoid(g_t)
         c = -np.expm1(g_t - g_s)
         sigma_t = np.sqrt(sigma2(g_t))
-        z_s = np.sqrt(a / b) * (z_t - sigma_t * c * eps_hat) + np.sqrt((1.0 - a) * c) * eps
+        z_s = np.sqrt(a / b) * (z_t - sigma_t * c * eps_hat_cond) + np.sqrt((1.0 - a) * c) * eps
 
         return z_s
