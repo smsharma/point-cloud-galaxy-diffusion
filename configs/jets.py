9af9d0e20b4d633c589ccf370648108b8533ba01
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-02-09 09:49:32 -0500

    Launched longer jets run

--- configs/jets.py
+++ configs/jets.py
@@ -16,12 +16,12 @@ def get_config():
 
     # Vartiational diffusion model
     config.vdm = vdm = ml_collections.ConfigDict()
-    vdm.timesteps = 1000
-    vdm.d_hidden_encoding = 256
+    vdm.timesteps = 500
+    vdm.d_hidden_encoding = 128
     vdm.n_encoder_layers = 5
     vdm.d_embedding = 8
     vdm.embed_context = True
-    vdm.n_classes = 2
+    vdm.n_classes = 3
 
     # Transformer score model
     config.transformer = transformer = ml_collections.ConfigDict()
@@ -29,24 +29,24 @@ def get_config():
     transformer.n_inducing_points = 500
     transformer.d_model = 256
     transformer.d_mlp = 1024
-    transformer.n_transformer_layers = 5
+    transformer.n_transformer_layers = 6
     transformer.n_heads = 4
 
     # Training
     config.training = training = ml_collections.ConfigDict()
     training.half_precision = False
     training.batch_size = 128  # Must be divisible by number of devices; this is the total batch size, not per-device
-    training.n_train_steps = 100_000
-    training.warmup_steps = 5000
+    training.n_train_steps = 1_200_000
+    training.warmup_steps = 10_000
     training.log_every_steps = 100
-    training.save_every_steps = 5000
+    training.save_every_steps = 10_000
 
     # Data
     config.data = data = ml_collections.ConfigDict()
     data.dataset = "jetnet"
     data.n_particles = 150  # Select the first n_particles particles
     data.n_features = 3  # Select the first n_features features
-    data.kwargs = {"jet_type": ["q", "g"]}
+    data.kwargs = {"jet_type": ["q", "g", "t"]}
 
     # Optimizer (AdamW)
     config.optim = optim = ml_collections.ConfigDict()
