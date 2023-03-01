374fffb30e76fdec30763ae72d965e6d5a8732f5
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-03-01 17:29:25 -0500

    Launched new nbody runs with just position

--- configs/nbody.py
+++ configs/nbody.py
@@ -37,7 +37,7 @@ def get_config():
     config.training = training = ml_collections.ConfigDict()
     training.half_precision = False
     training.batch_size = 16  # Must be divisible by number of devices; this is the total batch size, not per-device
-    training.n_train_steps = 301_000
+    training.n_train_steps = 501_000
     training.warmup_steps = 5_000
     training.log_every_steps = 100
     training.save_every_steps = 20_000
@@ -46,7 +46,7 @@ def get_config():
     config.data = data = ml_collections.ConfigDict()
     data.dataset = "nbody"
     data.n_particles = 5000  # Select the first n_particles particles
-    data.n_features = 7  # Select the first n_features features
+    data.n_features = 3  # Select the first n_features features
     data.kwargs = {}
 
     # Optimizer (AdamW)
