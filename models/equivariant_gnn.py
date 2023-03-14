a8b30cfdcd2c0f2901ecff0f8069f21215126e9f
Carol Cuesta <cuestalz@mit.edu>
2023-03-14 14:34:17 -0400

    added nequip as equivariant gnn

---
+++ models/equivariant_gnn.py
@@ -0,0 +1,178 @@
+from typing import Callable
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+import e3nn_jax as e3nn
+
+" Implementaiton of https://arxiv.org/pdf/2101.03164.pdf "
+" following https://github.com/mariogeiger/nequip-jax/tree/main"
+
+
+class NEQUIP(nn.Module):
+    use_node_features: bool = True
+    target_irreps: e3nn.Irreps = e3nn.Irreps("2x1o + 0e")
+    n_layers: int = 3
+
+    @nn.compact
+    def __call__(self, positions: jnp.array, node_features: jnp.array, senders: jnp.array, receivers: jnp.array)->jnp.array:
+        """ Forward pass of a nequip graph network
+
+        Args:
+            positions (jnp.array): 3D positions  
+            node_features (jnp.array): node features 
+            senders (jnp.array): senders of the edges 
+            receivers (jnp.array): receivers of the edges 
+
+        Returns:
+            jnp.array: new positions and node features 
+        """
+        vectors = e3nn.IrrepsArray("1o", positions[receivers] - positions[senders])
+        for l in range(self.n_layers):
+            print(f'Layer {l}')
+            layer = NEQUIPLayer(
+                avg_num_neighbors=1.0,
+                target_irreps=self.target_irreps,
+            )
+            node_features = layer(
+                vectors,
+                node_features,
+                senders=senders,
+                receivers=receivers,
+            )
+        displacements, node_features = (
+            node_features.slice_by_mul[:1],
+            node_features.slice_by_mul[1:],
+        )
+        return positions + displacements, node_features
+
+
+class NEQUIPLayer(nn.Module):
+    avg_num_neighbors: float
+    sh_lmax: int = 3
+    target_irreps: e3nn.Irreps = e3nn.Irreps("2x1o + 0e")
+    even_activation: Callable[[jnp.ndarray], jnp.ndarray] = jax.nn.swish
+    odd_activation: Callable[[jnp.ndarray], jnp.ndarray] = jax.nn.tanh
+    mlp_activation: Callable[[jnp.ndarray], jnp.ndarray] = jax.nn.swish
+    mlp_n_hidden: int = 64
+    mlp_n_layers: int = 2
+    n_radial_basis: int = 8
+
+    @nn.compact
+    def __call__(
+        self,
+        vectors: e3nn.IrrepsArray,  # [n_edges, 3]
+        node_feats: e3nn.IrrepsArray,  # [n_nodes, irreps]
+        senders: jnp.ndarray,  # [n_edges]
+        receivers: jnp.ndarray,  # [n_edges]
+    ):
+        n_edge = vectors.shape[0]
+        n_node = node_feats.shape[0]
+        assert vectors.shape == (n_edge, 3)
+        assert node_feats.shape == (n_node, node_feats.irreps.dim)
+        assert senders.shape == (n_edge,)
+        assert receivers.shape == (n_edge,)
+
+        # target irreps plus extra scalars for the gate activation
+        target_irreps = e3nn.Irreps(self.target_irreps)
+        irreps = target_irreps + target_irreps.filter(
+            drop="0e + 0o"
+        ).num_irreps * e3nn.Irreps("0e")
+
+        print('irreps = ', irreps)
+
+        self_connection = e3nn.flax.Linear(
+            irreps,
+        )(
+            node_feats
+        )  # [n_nodes, feature * target_irreps]
+
+        node_feats = e3nn.flax.Linear(node_feats.irreps, name="linear_up")(node_feats)
+
+        print('right before convolution')
+        node_feats = MessagePassingConvolution(
+            self.avg_num_neighbors,
+            irreps,
+            self.mlp_activation,
+            self.mlp_n_hidden,
+            self.mlp_n_layers,
+            self.n_radial_basis,
+            self.sh_lmax,
+        )(vectors, node_feats, senders, receivers)
+        print('right after convolution')
+
+        node_feats = e3nn.flax.Linear(irreps, name="linear_down")(node_feats)
+
+        node_feats = node_feats + self_connection  # [n_nodes, irreps]
+
+        node_feats = e3nn.gate(
+            node_feats,
+            even_act=self.even_activation,
+            even_gate_act=self.even_activation,
+            odd_act=self.odd_activation,
+            odd_gate_act=self.odd_activation,
+        )
+
+        assert node_feats.irreps == target_irreps
+        assert node_feats.shape == (n_node, target_irreps.dim)
+        return node_feats
+
+
+class MessagePassingConvolution(nn.Module):
+    avg_num_neighbors: float
+    target_irreps: e3nn.Irreps
+    activation: Callable[[jnp.ndarray], jnp.ndarray] = jax.nn.swish
+    mlp_n_hidden: int = 64
+    mlp_n_layers: int = 2
+    n_radial_basis: int = 8
+    sh_lmax: int = 3
+
+    @nn.compact
+    def __call__(
+        self,
+        vectors: e3nn.IrrepsArray,  # [n_edges, 3]
+        node_feats: e3nn.IrrepsArray,  # [n_nodes, irreps]
+        senders: jnp.ndarray,  # [n_edges, ]
+        receivers: jnp.ndarray,  # [n_edges, ]
+    ) -> e3nn.IrrepsArray:
+        messages = node_feats[senders]
+        # Angular part
+        messages = e3nn.concatenate(
+            [
+                messages.filter(self.target_irreps),
+                e3nn.tensor_product(
+                    messages,
+                    e3nn.spherical_harmonics(
+                        [l for l in range(1, self.sh_lmax + 1)],
+                        vectors,
+                        normalize=True,
+                        normalization="component",
+                    ),
+                    filter_ir_out=self.target_irreps,
+                ),
+            ]
+        ).regroup()  # [n_edges, irreps]
+
+        # Radial part
+        lengths = e3nn.norm(vectors).array  # [n_edges, 1]
+        mix = e3nn.flax.MultiLayerPerceptron(
+            self.mlp_n_layers * (self.mlp_n_hidden,) + (messages.irreps.num_irreps,),
+            self.activation,
+            output_activation=False,
+        )(
+            jnp.where(
+                lengths == 0.0,  # discard 0 length edges that come from graph padding
+                0.0,
+                e3nn.bessel(lengths[:, 0], self.n_radial_basis)
+                * e3nn.poly_envelope(5, 2)(lengths),
+            )
+        )  # [n_edges, num_irreps]
+
+        # Product of radial and angular part
+        messages = messages * mix  # [n_edges, irreps]
+
+        # Message passing
+        zeros = e3nn.IrrepsArray.zeros(
+            messages.irreps, node_feats.shape[:1], messages.dtype
+        )
+        node_feats = zeros.at[receivers].add(messages)  # [n_nodes, irreps]
+        return node_feats / jnp.sqrt(self.avg_num_neighbors)
--- models/scores.py
+++ models/scores.py
@@ -9,6 +9,7 @@ import e3nn_jax as e3nn
 from models.transformer import Transformer
 from models.gnn import GraphConvNet
 from models.equivariant_transformer import EquivariantTransformer
+from models.equivariant_gnn import NEQUIP 
 from models.mlp import MLP
 
 from models.graph_utils import nearest_neighbors
@@ -19,18 +20,28 @@ class TransformerScoreNet(nn.Module):
     """Transformer score network."""
 
     d_t_embedding: int = 32
-    score_dict: dict = dataclasses.field(default_factory=lambda: {"d_model": 256, "d_mlp": 512, "n_layers": 4, "n_heads": 4})
+    score_dict: dict = dataclasses.field(
+        default_factory=lambda: {
+            "d_model": 256,
+            "d_mlp": 512,
+            "n_layers": 4,
+            "n_heads": 4,
+        }
+    )
 
     @nn.compact
     def __call__(self, z, t, conditioning, mask):
-
         assert np.isscalar(t) or len(t.shape) == 0 or len(t.shape) == 1
         t = t * np.ones(z.shape[0])  # Ensure t is a vector
 
-        t_embedding = get_timestep_embedding(t, self.d_t_embedding)  # Timestep embeddings
+        t_embedding = get_timestep_embedding(
+            t, self.d_t_embedding
+        )  # Timestep embeddings
 
         if conditioning is not None:
-            cond = np.concatenate([t_embedding, conditioning], axis=1)  # Concatenate with conditioning context
+            cond = np.concatenate(
+                [t_embedding, conditioning], axis=1
+            )  # Concatenate with conditioning context
         else:
             cond = t_embedding
 
@@ -52,18 +63,29 @@ class GraphScoreNet(nn.Module):
     """Graph-convolutional score network."""
 
     d_t_embedding: int = 32
-    score_dict: dict = dataclasses.field(default_factory=lambda: {"k": 20, "num_mlp_layers": 4, "latent_size": 128, "skip_connections": True, "message_passing_steps": 4})
+    score_dict: dict = dataclasses.field(
+        default_factory=lambda: {
+            "k": 20,
+            "num_mlp_layers": 4,
+            "latent_size": 128,
+            "skip_connections": True,
+            "message_passing_steps": 4,
+        }
+    )
 
     @nn.compact
     def __call__(self, z, t, conditioning, mask):
-
         assert np.isscalar(t) or len(t.shape) == 0 or len(t.shape) == 1
         t = t * np.ones(z.shape[0])  # Ensure t is a vector
 
-        t_embedding = get_timestep_embedding(t, self.d_t_embedding)  # Timestep embeddings
+        t_embedding = get_timestep_embedding(
+            t, self.d_t_embedding
+        )  # Timestep embeddings
 
         if conditioning is not None:
-            cond = np.concatenate([t_embedding, conditioning], axis=1)  # Concatenate with conditioning context
+            cond = np.concatenate(
+                [t_embedding, conditioning], axis=1
+            )  # Concatenate with conditioning context
         else:
             cond = t_embedding
 
@@ -74,15 +96,17 @@ class GraphScoreNet(nn.Module):
         k = self.score_dict["k"]
         n_pos_features = self.score_dict["n_pos_features"]
 
-        sources, targets = jax.vmap(nearest_neighbors, in_axes=(0, None))(z[..., :n_pos_features], k, mask=mask)
+        sources, targets = jax.vmap(nearest_neighbors, in_axes=(0, None))(
+            z[..., :n_pos_features], k, mask=mask
+        )
         n_batch = z.shape[0]
         graph = jraph.GraphsTuple(
-            n_node=(mask.sum(-1)[:, None]).astype(np.int32), 
-            n_edge=np.array(n_batch * [[k]]), 
-            nodes=z, 
-            edges=None, 
-            globals=cond, 
-            senders=sources, 
+            n_node=(mask.sum(-1)[:, None]).astype(np.int32),
+            n_edge=np.array(n_batch * [[k]]),
+            nodes=z,
+            edges=None,
+            globals=cond,
+            senders=sources,
             receivers=targets,
         )
 
@@ -106,14 +130,17 @@ class EquivariantTransformereNet(nn.Module):
 
     @nn.compact
     def __call__(self, z, t, conditioning, mask):
-
         assert np.isscalar(t) or len(t.shape) == 0 or len(t.shape) == 1
         t = t * np.ones(z.shape[0])  # Ensure t is a vector
 
-        t_embedding = get_timestep_embedding(t, self.d_t_embedding)  # Timestep embeddings
+        t_embedding = get_timestep_embedding(
+            t, self.d_t_embedding
+        )  # Timestep embeddings
 
         if conditioning is not None:
-            cond = np.concatenate([t_embedding, conditioning], axis=1)  # Concatenate with conditioning context
+            cond = np.concatenate(
+                [t_embedding, conditioning], axis=1
+            )  # Concatenate with conditioning context
         else:
             cond = t_embedding
 
@@ -126,14 +153,22 @@ class EquivariantTransformereNet(nn.Module):
         n_pos_features = self.score_dict["n_pos_features"]
 
         # Isolate positions, velocities, and masses; get nearest neighbor edges.
-        pos, vel, mass = z[..., :n_pos_features], z[..., n_pos_features : 2 * n_pos_features], z[..., 2 * n_pos_features :]
-        sources, targets = jax.vmap(nearest_neighbors, in_axes=(0, None))(z[..., :n_pos_features], k)
+        pos, vel, mass = (
+            z[..., :n_pos_features],
+            z[..., n_pos_features : 2 * n_pos_features],
+            z[..., 2 * n_pos_features :],
+        )
+        sources, targets = jax.vmap(nearest_neighbors, in_axes=(0, None))(
+            z[..., :n_pos_features], k
+        )
 
         feat = np.concatenate([vel, mass], -1)
 
         # Position and feature irreps arrays. Add the mass to the conditioning vectors.
         pos = e3nn.IrrepsArray("1o", pos)
-        feat = e3nn.IrrepsArray(f"1o + {d_cond}x0e", np.concatenate([vel, mass + cond[:, None, :]], -1))
+        feat = e3nn.IrrepsArray(
+            f"1o + {d_cond}x0e", np.concatenate([vel, mass + cond[:, None, :]], -1)
+        )
 
         # Make copy of score dict since original cannot be in-place modified; remove `k` argument before passing to Net
         score_dict = dict(self.score_dict)
@@ -141,7 +176,72 @@ class EquivariantTransformereNet(nn.Module):
         score_dict.pop("score", None)
         score_dict.pop("n_pos_features", None)
 
-        pos, feat = jax.vmap(EquivariantTransformer(irreps_out="1o + 0e", **score_dict))(pos, feat, sources, targets)
+        pos, feat = jax.vmap(
+            EquivariantTransformer(irreps_out="1o + 0e", **score_dict)
+        )(pos, feat, sources, targets)
+
+        h = np.concatenate([pos.array, feat.array], -1)
+
+        return z + h
+
+
+class EquivariantGNNNet(nn.Module):
+    """Equivariant GNN network. NOTE: Does not currently support masking."""
+
+    d_t_embedding: int = 32
+    score_dict: dict = dataclasses.field(
+        default_factory=lambda: {"k": 20, "sh_lmax": 3}
+    )
+
+    @nn.compact
+    def __call__(self, z, t, conditioning, mask):
+        assert np.isscalar(t) or len(t.shape) == 0 or len(t.shape) == 1
+        t = t * np.ones(z.shape[0])  # Ensure t is a vector
+
+        t_embedding = get_timestep_embedding(
+            t, self.d_t_embedding
+        )  # Timestep embeddings
+
+        if conditioning is not None:
+            cond = np.concatenate(
+                [t_embedding, conditioning], axis=1
+            )  # Concatenate with conditioning context
+        else:
+            cond = t_embedding
+
+        d_cond = cond.shape[-1]  
+        cond = MLP([d_cond * 4, d_cond * 4, d_cond])(cond)
+        k = self.score_dict["k"]
+        n_pos_features = self.score_dict["n_pos_features"]
+
+        # Isolate positions, velocities, and masses; get nearest neighbor edges.
+        pos, vel, mass = (
+            z[..., :n_pos_features],
+            z[..., n_pos_features : 2 * n_pos_features],
+            z[..., 2 * n_pos_features :],
+        )
+        sources, targets = jax.vmap(nearest_neighbors, in_axes=(0, None))(
+            z[..., :n_pos_features], k
+        )
+
+        feat = np.concatenate([vel, mass], -1)
+
+        # Position and feature irreps arrays. Add the mass to the conditioning vectors.
+        # TODO: Change how conditioning is introduced
+        pos = e3nn.IrrepsArray("1o", pos)
+        feat = e3nn.IrrepsArray(
+            f"1o + {d_cond}x0e", np.concatenate([vel, mass + cond[:, None, :]], -1)
+        )
+
+        # Make copy of score dict since original cannot be in-place modified; remove `k` argument before passing to Net
+        score_dict = dict(self.score_dict)
+        score_dict.pop("k", None)
+        score_dict.pop("score", None)
+        score_dict.pop("n_pos_features", None)
+
+        pos, feat = jax.vmap(NEQUIP(target_irreps="1o + 0e", **score_dict))(
+            pos, feat, sources, targets
+        )
 
         h = np.concatenate([pos.array, feat.array], -1)
 
