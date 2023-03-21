db4c1e4398eb9b879375a1162cd133b18375ae2d
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-03-20 21:24:58 -0400

    Working radius graph in nb

--- models/gnn.py
+++ models/gnn.py
@@ -6,6 +6,7 @@ import jraph
 from models.graph_utils import add_graphs_tuples
 from models.mlp import MLP
 
+
 def get_node_mlp_updates(mlp_feature_sizes: int) -> Callable:
     """Get a node MLP update  function
 
@@ -67,25 +68,11 @@ def get_edge_mlp_updates(mlp_feature_sizes: int) -> Callable:
             jnp.ndarray: updated edge features
         """
         if edges is not None:
-            inputs = jnp.concatenate(
-                [
-                    edges,
-                    senders,
-                    receivers,
-                    globals,
-                ],
-                axis=1,
-            )
+            inputs = jnp.concatenate([edges, senders, receivers, globals], axis=1)
         else:
-            inputs = jnp.concatenate(
-                [
-                    senders,
-                    receivers,
-                    globals,
-                ],
-                axis=1,
-            )
+            inputs = jnp.concatenate([senders, receivers, globals], axis=1)
         return MLP(mlp_feature_sizes)(inputs)
+
     return update_fn
 
 
@@ -113,28 +100,20 @@ class GraphConvNet(nn.Module):
         # We will first linearly project the original node features as 'embeddings'.
         embedder = jraph.GraphMapFeatures(embed_node_fn=nn.Dense(self.latent_size))
         processed_graphs = embedder(graphs)
-        processed_graphs = processed_graphs._replace(
-            globals=processed_graphs.globals.reshape(1, -1),
-        )
+        processed_graphs = processed_graphs._replace(globals=processed_graphs.globals.reshape(1, -1))
         mlp_feature_sizes = [self.latent_size] * self.num_mlp_layers
         update_node_fn = get_node_mlp_updates(mlp_feature_sizes)
         update_edge_fn = get_edge_mlp_updates(mlp_feature_sizes)
+
         # Now, we will apply the GCN once for each message-passing round.
         for _ in range(self.message_passing_steps):
-            graph_net = jraph.GraphNetwork(
-                update_node_fn=update_node_fn,
-                update_edge_fn=update_edge_fn,
-            )
+            graph_net = jraph.GraphNetwork(update_node_fn=update_node_fn, update_edge_fn=update_edge_fn)
             if self.skip_connections:
-                processed_graphs = add_graphs_tuples(
-                    graph_net(processed_graphs), processed_graphs
-                )
+                processed_graphs = add_graphs_tuples(graph_net(processed_graphs), processed_graphs)
             else:
                 processed_graphs = graph_net(processed_graphs)
 
             if self.layer_norm:
-                processed_graphs = processed_graphs._replace(
-                    nodes=nn.LayerNorm()(processed_graphs.nodes),
-                )
+                processed_graphs = processed_graphs._replace(nodes=nn.LayerNorm()(processed_graphs.nodes))
         decoder = jraph.GraphMapFeatures(embed_node_fn=nn.Dense(in_features))
         return decoder(processed_graphs)
