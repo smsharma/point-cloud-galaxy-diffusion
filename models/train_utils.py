1e77d2703392f2bc2dd3cf2a6cf53a4ca15d8758
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-08-11 15:23:02 -0400

    Debugged unconditional dropout; new data dir path

--- models/train_utils.py
+++ models/train_utils.py
@@ -21,22 +21,18 @@ def create_input_iter(ds):
     return it
 
 
-@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4))
+@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5, 6))
 def train_step(state, batch, rng, model, loss_fn, unconditional_dropout=False, p_uncond=0.0):
     """Train for a single step."""
     x, conditioning, mask = batch
 
     # Unconditional dropout rng
     rng, rng_uncond = jax.random.split(rng)
+    random_nums = jax.random.uniform(rng_uncond, conditioning.shape[:1]).reshape(-1, 1)
 
     # Set a fraction p_uncond of conditioning vectors to zero if unconditional_dropout is True
     if conditioning is not None and unconditional_dropout:
-        conditioning = jax.lax.cond(
-            jax.random.uniform(rng_uncond, (1,)) < p_uncond,
-            lambda _: np.zeros_like(conditioning),
-            lambda _: conditioning,
-            operand=None,
-        )
+        conditioning = np.where(random_nums < p_uncond, np.zeros_like(conditioning), conditioning)
 
     loss, grads = jax.value_and_grad(loss_fn)(state.params, model, rng, x, conditioning, mask)
     grads = jax.lax.pmean(grads, "batch")
