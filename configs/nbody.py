9226ca6a9a35b3158bf76d8a74c5202ac57e9587
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-09-05 14:27:16 -0400

    Launch jobs with new configs

--- configs/nbody.py
+++ configs/nbody.py
@@ -68,7 +68,7 @@ def get_config():
     # Training
     config.training = training = ml_collections.ConfigDict()
     training.half_precision = False
-    training.batch_size = 4  # Must be divisible by number of devices; this is the total batch size, not per-device
+    training.batch_size = 32  # Must be divisible by number of devices; this is the total batch size, not per-device
     training.n_train_steps = 301_000
     training.warmup_steps = 5_000
     training.log_every_steps = 100
