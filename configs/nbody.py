e3adc8a0326667ea7e2cfbf409fae8da05f71c88
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-09-11 14:38:19 -0400

    More runs with pairnorm

--- configs/nbody.py
+++ configs/nbody.py
@@ -58,13 +58,13 @@ def get_config():
     score.latent_size = 16
     score.hidden_size = 128
     score.skip_connections = True
-    score.message_passing_steps = 16
+    score.message_passing_steps = 8
     score.attention = True
     score.shared_weights = False  # GNN shares weights across message passing steps; Doesn't work yet because of flax quirks
     score.use_edges = True
     score.use_pbc = True
     score.graph_construction = "pairwise_dist"  # "kd_tree" or "pairwise_dist"
-    score.norm = "layer"  # "pair" or "layer" for LayerNorm or PairNorm. Otherwise, no normalization.
+    score.norm = "pair"  # "pair" or "layer" for LayerNorm or PairNorm. Otherwise, no normalization.
 
     # Training
     config.training = training = ml_collections.ConfigDict()
@@ -92,7 +92,7 @@ def get_config():
 
     # Optimizer (AdamW)
     config.optim = optim = ml_collections.ConfigDict()
-    optim.learning_rate = 1e-3
+    optim.learning_rate = 3e-4
     optim.weight_decay = 1e-4
 
     config.seed = 52
